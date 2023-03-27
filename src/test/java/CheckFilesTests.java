30c7908e8976d3ec08a7a738ce491eef10949585
Kolesnikov Vladislav <kolesnikov@aqsi.ru>
2023-03-27 16:16:40 +0300

    added check faly witch type csv pdf xlsx

--- src/test/java/CheckFilesTests.java
+++ src/test/java/CheckFilesTests.java
@@ -1,10 +1,7 @@
 import com.codeborne.pdftest.PDF;
 import com.codeborne.xlstest.XLS;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.gson.Gson;
-import com.google.gson.JsonObject;
 import com.opencsv.CSVReader;
-import com.google.common.io.Files;
 import data.DataForJson;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -33,33 +30,6 @@ public class CheckFilesTests extends Exception{
         }
     }
     private final ClassLoader cl = CheckFilesTests.class.getClassLoader();
-    @Test
-    void checkFillingAllFiles() throws Exception{
-        try (InputStream is = cl.getResourceAsStream("AllFiles.zip");
-             ZipInputStream zis = new ZipInputStream(is)) {
-            ZipEntry entry;
-            while ((entry = zis.getNextEntry()) != null) {
-                switch (Files.getFileExtension(entry.getName())) {
-                    case "xlsx" -> {
-                        XLS xls = new XLS(zis);
-                        Assertions.assertTrue(xls.excel.getSheetAt(0).getRow(0).getCell(5).
-                                        getStringCellValue().startsWith("xls"),
-                                         "oops xlsx pars error");
-                    }
-                    case "pdf" -> {
-                        PDF pdf = new PDF(zis);
-                        Assertions.assertEquals("Typora", pdf.creator , "oops pdf pars error");
-                    }
-                    case "csv" -> {
-                        CSVReader csv = new CSVReader(new InputStreamReader(zis));
-                        List<String[]> content = csv.readAll();
-                        Assertions.assertArrayEquals(new String[]{"csv"}, content.get(4),
-                                "csv parsed failed");
-                    }
-                }
-            }
-        }
-    }
 
     @Test
     void checkJsonStructure()  throws Exception {
@@ -81,4 +51,49 @@ public class CheckFilesTests extends Exception{
             Assertions.assertEquals(null, dataForJson.address, "ERROR ADRESS CLIENT");
         }
     }
+    void checkFillingXlsxFile() throws Exception{
+        try (InputStream is = cl.getResourceAsStream("AllFiles.zip");
+             ZipInputStream zis = new ZipInputStream(is)) {
+            ZipEntry entry;
+            while ((entry = zis.getNextEntry()) != null) {
+                if (entry.getName().contains(".xlsx")) {
+                    XLS xls = new XLS(zis);
+                    Assertions.assertTrue(xls.excel.getSheetAt(0).getRow(0).getCell(5).
+                                    getStringCellValue().startsWith("xls"),
+                            "oops xlsx pars error");
+                }
+            }
+        }
+    }
+
+    @Test
+    void checkFillingPdfFile() throws Exception{
+        try (InputStream is = cl.getResourceAsStream("AllFiles.zip");
+             ZipInputStream zis = new ZipInputStream(is)) {
+            ZipEntry entry;
+            while ((entry = zis.getNextEntry()) != null) {
+                if (entry.getName().contains(".pdf")) {
+                    PDF pdf = new PDF(zis);
+                    Assertions.assertEquals("Typora", pdf.creator, "oops pdf pars error");
+                }
+            }
+        }
+    }
+
+    @Test
+    void checkFillingCsvFile() throws Exception{
+        try (InputStream is = cl.getResourceAsStream("AllFiles.zip");
+             ZipInputStream zis = new ZipInputStream(is)) {
+            CSVReader csv = new CSVReader(new InputStreamReader(zis));
+            ZipEntry entry;
+            while ((entry = zis.getNextEntry()) != null) {
+                if (entry.getName().contains(".csv")) {
+                    List<String[]> content = csv.readAll();
+                    Assertions.assertArrayEquals(new String[]{"csv"}, content.get(4),
+                            "csv parsed failed");
+                }
+            }
+        }
+    }
+
 }
